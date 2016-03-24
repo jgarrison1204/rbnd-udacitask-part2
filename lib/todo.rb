001c75b55d10641ecc86a58bb2c4cb54a2ce1a00
Jacob Garrison <jgarrison1204@gmail.com>
2016-03-24 16:49:17 -0700

    Updated format_priority method

--- lib/todo.rb
+++ lib/todo.rb
@@ -5,21 +5,18 @@ class TodoItem
   def initialize(description, options={})
     @description = description
     @due = options[:due] ? Date.parse(options[:due]) : options[:due]
-    @priority = options[:priority]
+    @priority = options[:priority].to_s
   end
-  #def format_description
-  #  description.ljust(25)
+  #def format_priority
+    #value = " ⇧" if @priority == "high"
+    #value = " ⇨" if @priority == "medium"
+    #value = " ⇩" if @priority == "low"
+    #value = "" if !@priority
+    #return value
   #end
-  def format_priority
-    value = " ⇧" if @priority == "high"
-    value = " ⇨" if @priority == "medium"
-    value = " ⇩" if @priority == "low"
-    value = "" if !@priority
-    return value
-  end
   def details
     format_description(@description) + "due: " +
     format_date(@due) +
-    format_priority
+    format_priority(@priority)
   end
 end
