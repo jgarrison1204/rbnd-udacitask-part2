a5774f140ad17568576d71256b318a8c744b0eb5
Jacob Garrison <jgarrison1204@gmail.com>
2016-03-25 14:59:22 -0700

    Added Chronic gem to enable various date entry types

--- lib/todo.rb
+++ lib/todo.rb
@@ -4,14 +4,13 @@ class TodoItem
 
   def initialize(description, options={})
     @description = description
-    @due = options[:due] ? Date.parse(options[:due]) : options[:due]
+    @due = options[:due] ? Chronic.parse(options[:due], ) : options[:due]
     @priority = options[:priority].to_s
   end
 
   def details
-    if @priority
     format_description(@description) + "due: " +
-    format_date(@due) +
+    format_date(@due, ) +
     format_priority(@priority)
   end
 end
