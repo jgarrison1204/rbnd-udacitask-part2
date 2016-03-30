dcb2fde8de9792a4ec4b18380934569fdf3fef68
Jacob Garrison <jgarrison1204@gmail.com>
2016-03-30 16:15:44 -0700

    Added terminal-table, multi-delete method, and update_priority method

--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -27,11 +27,13 @@ class UdaciList
     end
   end
 
-  def delete(index)
-    @items.delete_at(index - 1)
-    nummber_of_items = @items.length
-    if nummber_of_items < index
-      raise UdaciListErrors::IndexExceedsListSize, "Oops, that item number doesn't exist."
+  #splat operator to allow multiple deletions at a time.
+  def delete(*index)
+    index.each do |item|
+      if @items.length < item
+        raise UdaciListErrors::IndexExceedsListSize, "Oops, that item number doesn't exist."
+      end
+      @items.delete_at(item - 1)
     end
   end
 
@@ -47,10 +49,36 @@ class UdaciList
   def filter(type)
     filter_by_type = @items.select {|filter| filter.type == type}
     if filter_by_type
-      format_filter(@title, type)
-      filter_by_type.map {|item_type| puts item_type.details}
+      @filter_by_type_details = filter_by_type.map {|item_type| item_type.details}
+      format_filter_by_type
     else
       "There are no #{type}s in #{@title}"
+    end
+  end
+
+  def format_filter_by_type
+    @table = Terminal::Table.new do |t|
+      t << @filter_by_type_details[0].split(",")
+      t << :separator
+      t << @filter_by_type_details[1].split(",")
+      t << :separator
+      t << @filter_by_type_details[2].split(",")
+    end
+    print_format_filter_by_type
+  end
+
+  def print_format_filter_by_type
+    puts
+    @table.title = "List of items for events"
+    @table.headings = ['Event', 'Date']
+    puts @table
+  end
+
+  def update_priority(item, update)
+    if @@priority_levels.include? update
+      @items.map {|item_in_array| item_in_array.update_priority_status(update) if item_in_array.description == item}
+    else
+      raise UdaciListErrors::InvalidPriorityValue, "Oops, that priority level doesn't exist."
     end
   end
 end
